9d5bb535cb2378f35fa4d647c6cc132a224ecdc8
Redwan Mahmud <redwan.mahmud07@gmail.com>
2017-10-22 02:37:53 -0700

    all functionalities entered, need to fix one bug

--- frontend.py
+++ frontend.py
@@ -32,10 +32,26 @@ def add_entry():
     list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),ISBN_text.get()))
 
 def get_selected_row(event):
+    global selected_tuple
     index= list1.curselection()[0]
     selected_tuple = list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4])
 
-    print(selected_tuple)
+    #e1.delete(0,END)
+    #e1.insert(END,selected_tuple[1])
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+
+def update_command():
+    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),ISBN_text.get())
 
 
 window = Tk()
@@ -93,14 +109,15 @@ b2.grid(row = 3, column = 3 )
 b3 = Button(window,text="Add entry", width = 12,command = add_entry)
 b3.grid(row = 4, column = 3 )
 
-b4 = Button(window,text="Update selected", width = 12)
+b4 = Button(window,text="Update selected", width = 12, command = update_command)
 b4.grid(row = 5, column = 3 )
 
-b5 = Button(window,text="Delete selected", width = 12)
+b5 = Button(window,text="Delete selected", width = 12, command = delete_command)
 b5.grid(row = 6, column = 3 )
 
-b6 = Button(window,text="Close", width = 12)
+b6 = Button(window,text="Close", width = 12,comman = window.destroy)
 b6.grid(row = 7, column = 3 )
 
+window.wm_title("Bookstore")
 
 window.mainloop()
